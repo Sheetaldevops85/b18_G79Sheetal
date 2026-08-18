a20b7066a78586a3563d981a9b31d27f1d66ea5d
Sheetal Agarwal <sheetalag2010@gmail.com>
2026-08-18 13:10:14 -0400

    storagedel

--- environment/prepod/main.tf
+++ environment/prepod/main.tf
@@ -6,8 +6,3 @@ module "rg"{
   source = "../../modules/resource_group"
     rg = var.rg1
 }
-module "storage"{
-    source = "../../modules/storage_account"
-    depends_on = [module.rg]
-    storage =var.sto1
-}
